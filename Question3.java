5df4d8a5560f8f63b62623ed839fa756f282801f
aizakhurram <aizakhurram@github.com>
2023-10-10 12:15:16 +0500

    final

--- Question3.java
+++ Question3.java
@@ -1,16 +1,15 @@
 import java.util.ArrayList;
-
 public class Question3 {
      public static void mergeList(ArrayList<int[]> list){
         if (list == null) {
             return;
         }
-        for(int i=1, y=0; i<list.size(); i++){
+        int y=0;
+        for(int i=1; i<list.size(); i++){
             int[] interval= list.get(y);
             int[] next=list.get(i);
             if(interval[interval.length-1]>=next[0]){
                 interval[interval.length-1]=Math.max(next[next.length-1],interval[interval.length-1]);
-            
             }
             else{
             y++;
@@ -18,18 +17,13 @@ public class Question3 {
            
         }
         }
-        for(int i=0, y=1; i<list.size(); i++,  y++){
-            if(y>=list.size()){
-                break;
-            }
-            
-            if(list.get(i)==list.get(y) && i!=y){
-                list.remove(y);
-                
-               
-            }
-          
-        
+        int i = 1;
+       while (i < list.size()) {
+         if (list.get(i - 1)[1] >= list.get(i)[0]) {
+          list.remove(i);
+          } else {
+          i++;
+         }
         }
        
       }
@@ -42,7 +36,7 @@ public class Question3 {
     public static void main(String[] args){
         ArrayList<int[]> list= new ArrayList<>();
         list.add(new int[]{1, 7});
-        list.add(new int[]{2, 8});
+        list.add(new int[]{2, 6});
         list.add(new int[]{8, 10});
         list.add(new int[]{15, 18});
 
